10a9cca0c4e2b416a729e3147ab1f14af03fa034
DamianoD88 <dispe88@live.it>
2021-06-18 17:36:04 +0200

    add end esercizio

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let a = parseInt(prompt('inserisci number da 1 a 8'));
+let b = parseInt(prompt('inserisci number superiore a quello precedente'));
+
+
+const newArray = array.filter((number) =>{
+    if(number >= a && number <= b){
+        return true
+    }
+    
+    return false;
+});
+
+console.log(newArray);
+
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
+//Es: 
+
+
